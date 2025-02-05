8b1d6448db0ec7f409f1eed728acd0a781574e21
selrahcsan <charlesstna@proton.me>
2025-02-05 11:03:31 -0300

    Corrigido tamanho do vetor

--- Exemplos/02/main.c
+++ Exemplos/02/main.c
@@ -4,17 +4,17 @@
 #include <stdio.h>
 
 int main() {
-  int valoresTeclado[5];
+  int valoresTeclado[6];
   int contador;
 
-  for (contador = 0;  contador < 5 ; contador++){
-    printf("Digite o %d Número: ", contador+1);
+  for (contador = 0;  contador < 6 ; contador++){
+    printf("Digite o %d° Número: ", contador+1);
     scanf("%d", &valoresTeclado[contador]);
   }
 
   printf("\nvetor = ");
 
-  for (contador = 0; contador <5; contador++){
+  for (contador = 0; contador <6; contador++){
     printf("[%d]", valoresTeclado[contador]);
   }
   printf("\n");
